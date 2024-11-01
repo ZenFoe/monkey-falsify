95ef51a87eba5f54ebe9777783555ba1b2354a0c
ZenFoe <133615878+ZenFoe@users.noreply.github.com>
2024-11-01 19:35:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.config = {
   proxyLink: "https://aluu.xyz/en/", // proxy link, highly recommended you change it
   blanker: "byebye", // opens your proxy link in about blank
   leverofdoom: "no",
-  alternativeproxy: "https://nano-proxy.github.io/", // alternative Proxy
+  alternativeproxy: "https://anura.pro/", // alternative Proxy
   useproxy: "true" // Some schools have certain restrictions on proxies that can cause students to get in massive trouble. Ticking this to false changes the website to only have games & movies (not in current use)
 };
