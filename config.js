1e26eba6073ed831bed103b99a54b1bafcf2a2ef
ZenFoe <133615878+ZenFoe@users.noreply.github.com>
2024-11-01 19:09:28 -0400

    Randolph config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.config = {
   entryPhrase: "getmein",  // phrase to enter the real site
-  proxyLink: "https://example.com", // proxy link, highly recommended you change it
+  proxyLink: "https://nano-proxy.github.io/", // proxy link, highly recommended you change it
   blanker: "byebye", // opens your proxy link in about blank
   leverofdoom: "no",
   alternativeproxy: "https://youtube.com", // alternative Proxy
