6c2de07651e6ff157e9a6fbc8f32b8f10fbebadf
ZenFoe <133615878+ZenFoe@users.noreply.github.com>
2024-11-01 19:18:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ window.config = {
   blanker: "byebye", // opens your proxy link in about blank
   leverofdoom: "no",
   alternativeproxy: "https://youtube.com", // alternative Proxy
-  useproxy: "false" // Some schools have certain restrictions on proxies that can cause students to get in massive trouble. Ticking this to false changes the website to only have games & movies (not in current use)
+  useproxy: "true" // Some schools have certain restrictions on proxies that can cause students to get in massive trouble. Ticking this to false changes the website to only have games & movies (not in current use)
 };
